14e8b49140afa40b260c1cdc89ff8b39ae66615c
djhustla <149957180+djhustla@users.noreply.github.com>
2025-12-29 04:57:08 +0100

    Update SecurityLearningApplication.java

--- src/main/java/main/SecurityLearningApplication.java
+++ src/main/java/main/SecurityLearningApplication.java
@@ -1,10 +1,10 @@
 package main;
 
-import main.divers_services.EnvoiSms;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class SecurityLearningApplication {
@@ -13,16 +13,6 @@ public class SecurityLearningApplication {
         SpringApplication.run(SecurityLearningApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner testSms() {
-        return args -> {
-            System.out.println("--- Démarrage du test d'envoi SMS ---");
 
-            // Appelle ta méthode statique
-            // Assure-toi que le numéro est au format international (ex: +32...)
-            EnvoiSms.envoyerSms("+32498645488", "Le serveur Spring Boot est en ligne !");
 
-            System.out.println("--- Fin du test d'envoi SMS ---");
-        };
-    }
 }
